dac0202bda22e470cc458f6a59c1ac58040068f4
Andy <andyivanov2007@gmail.com>
2024-08-23 14:39:45 +0300

    ADD Menu.h

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Menu.cpp"
+#include "Menu.h"
 using namespace std;
 
 int main() {
---
+++ ConsoleApplication1/Menu.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Account.h"  
+
+int WelcomeMenu();
+void LoginMenu();
+Card* WhichOneCards(Account& Acc);
+void RegisterateMenu();
+void Menu(Account& Acc);
